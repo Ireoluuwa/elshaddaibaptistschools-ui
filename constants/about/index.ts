e229077ef93c2cadf1d5efad647bd5f2d9ea5ed4
Francis <offor919@gmail.com>
2026-04-18 11:50:09 -0700

    feat: export about page constants from new index file

--- constants/about/index.ts
+++ constants/about/index.ts
@@ -3,3 +3,6 @@ export * from "./roots";
 export * from "./principles";
 export * from "./principal";
 export * from "./aboutCTA";
+export * from "./coreValues";
+export * from "./history";
+export * from "./leadership";
