9212b09046cbf8d9ea670f036e3f001d728eede9
Francis <offor919@gmail.com>
2026-04-17 22:58:43 -0700

    change title color

--- components/news/NewsHeader.tsx
+++ components/news/NewsHeader.tsx
@@ -4,7 +4,7 @@ import { newsHeader } from "@/constants/news";
 const NewsHeader = () => {
   return (
     <div className="flex flex-col gap-3 pb-6 border-b border-[#e2e8f0]">
-      <h1 className="text-primary text-4xl lg:text-5xl font-black leading-tight tracking-tight">
+      <h1 className="text-secondary text-4xl lg:text-5xl font-black leading-tight tracking-tight">
         {newsHeader.title}
       </h1>
       <p className="text-[#64748b] text-lg font-normal">
