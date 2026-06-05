92db7fb248d94b3fae88b93f1b4cdaffa589202b
Dozie <offor919@gmail.com>
2026-06-06 00:11:19 +0100

    style: increase base font size of gallery header title to 3xl

--- components/gallery/GalleryHeader.tsx
+++ components/gallery/GalleryHeader.tsx
@@ -5,7 +5,7 @@ const GalleryHeader = () => {
   return (
     <section className="mx-auto max-w-7xl px-6 py-12 lg:px-20 lg:py-16">
       <div className="flex flex-col gap-6 text-center md:text-left">
-        <h1 className="text-2xl font-black tracking-tight text-secondary sm:text-5xl lg:text-6xl capitalize">
+        <h1 className="text-3xl font-black tracking-tight text-secondary sm:text-5xl lg:text-6xl capitalize">
           {galleryHeader.title}
         </h1>
         <p className="max-w-2xl text-sm md:text-lg text-[#64748b] leading-relaxed">
