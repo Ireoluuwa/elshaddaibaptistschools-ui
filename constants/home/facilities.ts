d513eb7b266a125fb361081ac55808c41926ad01
Dozie <offor919@gmail.com>
2026-06-05 23:28:00 +0100

    refactor: update hero and facilities image sources to local assets and remove redundant slides

--- constants/home/facilities.ts
+++ constants/home/facilities.ts
@@ -9,19 +9,19 @@ export const facilitiesData = [
   {
     title: "Modern Library",
     desc: "Over 10,000 resources for research and study.",
-    img: "https://lh3.googleusercontent.com/aida-public/AB6AXuDr9bXxPga9MlMOF061vhfj2825e5vZbdlIJkkCpmM8FscZoUh4kI4xhvJMLeur8q4q9Y1HS7u307ZQnkfFVmaTn8W5zHjQ2lNntHsmc0Hg2exoBCCFpyQEsRkAewgWh03B8LlfrFviiIGmOP8fkZg6WkULKe1CK1b1pv_pNbqHzPl4cYFipm8LFEm7WbNwmyUeIUqxN-hdK9_G-7I_slfdCN_baCTVlR8Iv_LxiNLH5pqRU_Zat_ViYXASgGw6sK59hQlJ5FepTQ",
+    img: "/banner1.jpeg",
     alt: "Modern Library with shelves of books",
   },
   {
     title: "Science Lab",
     desc: "Fully equipped Physics, Chemistry & Biology stations.",
-    img: "https://lh3.googleusercontent.com/aida-public/AB6AXuB3OSU_4ZJmtAEk0ukZp5oV6MsczT8Oq4OfDKoO4279f7vFvR85ozkcvFtwpT7eYL4z4mLy2Iyt1PHolp6IjSNotE5XYJT00zHTZdM7ikjNCqavwSpsXHvSLAAf8zq0Fokz8K0nxqP2OBkyTdrmC6j6QYfia5LqvGMEz_AMfgXUKalMkwscnlbzUykFytZqVYawWBdTSRrT9BRNQDDXPMNkwSnDED-PEqAIUuEvLRx2FhbW2Un6p0M4ud3kMc4Mgwg4p_tBEegprw",
+    img: "/banner2.jpeg",
     alt: "Science Lab with students performing experiments",
   },
   {
     title: "ICT Center",
     desc: "High-speed internet and coding workshops.",
-    img: "https://lh3.googleusercontent.com/aida-public/AB6AXuC5RKekcrgDUlDCtjwmHATDdnkUzfYr_3tVVQFoGwelzRbCVsWb8R_JL04PK3yRG9XAwG51E4On_sa7ifcxT7F7ZSbN2JPOBLhiC4BJtqngZRB6wsbr8v0a2VUydqrbNA1QjZxL2lYMecQufBvr6CP_RjhC31S2wzFHUUq-MPSlnpHItdgsFxsFYKfEjFhlPfDyGG_2KVmXoQToV5i4e3CDHvd1OHRd0magsSUeDZso5hhliU5tllUTomRjcQKReVx00tULB42iEQ",
+    img: "/banner3.jpeg",
     alt: "ICT Center with computers",
   },
 ];
